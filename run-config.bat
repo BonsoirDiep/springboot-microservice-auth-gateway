35bae835b2b023648650d195fe978fa28d92d8a0
diepnguyen2611 <hoancva86@gmail.com>
2024-03-20 17:38:14 +0700

    re-config

--- run-config.bat
+++ run-config.bat
@@ -1,3 +1,4 @@
 @ECHO OFF
+set PJDIR=%cd%
 cd config-service/target && start java -jar config-0.0.1-SNAPSHOT.jar
 cd ../../
--- run.bat
+++ run.bat
@@ -1,7 +1,8 @@
 @ECHO OFF
 cd discovery-server/target && start java -jar discovery-server-0.0.1-SNAPSHOT.jar
 cd ../../api-gateway/target && start java -jar api-gateway-0.0.1-SNAPSHOT.jar
+cd ../../auth-service/target && start java -jar auth-service-0.0.1-SNAPSHOT.jar
 cd ../../product-service/target && start java -jar product-service-0.0.1-SNAPSHOT.jar
-cd ../../order-service/target && start java -jar order-service-0.0.1-SNAPSHOT.jar
-cd ../../product-stock-service/target && start java -jar product-stock-service-0.0.1-SNAPSHOT.jar
+@REM cd ../../order-service/target && start java -jar order-service-0.0.1-SNAPSHOT.jar
+@REM cd ../../product-stock-service/target && start java -jar product-stock-service-0.0.1-SNAPSHOT.jar
 cd ../../
