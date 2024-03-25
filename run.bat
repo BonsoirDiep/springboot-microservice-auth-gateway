b57c6b3febf87906bd0900e0814985d95b72cfc7
diepnguyen2611 <hoancva86@gmail.com>
2024-03-25 10:23:51 +0700

    fix *.bat for local dev

--- run.bat
+++ run.bat
@@ -1,6 +1,4 @@
 @ECHO OFF
 cd auth-service/target && start java -jar auth-service-0.0.1-SNAPSHOT.jar
 cd ../../product-service/target && start java -jar product-service-0.0.1-SNAPSHOT.jar
-@REM cd ../../order-service/target && start java -jar order-service-0.0.1-SNAPSHOT.jar
-@REM cd ../../product-stock-service/target && start java -jar product-stock-service-0.0.1-SNAPSHOT.jar
 cd ../../
